81532cad036a30d933d05ccf23840126b5d4a117
oconnormjohn <52138129+oconnormjohn@users.noreply.github.com>
2025-10-26 18:06:50 +0000

    v40 > v10

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
-const CACHE = 'nativity-player-v40';
+const CACHE = 'nativity-player-v10';
 const FILES = [
   './',
   './index.html',
   './style.css',
-  './script.js?v=40',
+  './script.js?v=10',
   './manifest.json',
   './mp3/Away in a MangerMOC.mp3',
   './mp3/Little Donkey.mp3',
